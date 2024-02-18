072bb47416f7a6994451f2f1928fe8138b4f1aff
BetoPoli <160300167+BetoPoli@users.noreply.github.com>
2024-02-17 18:50:07 -0600

    Archivos Externos 2

--- Manejo_archivos.py
+++ Manejo_archivos.py
@@ -1,8 +1,18 @@
 from io import open 
 
-archivo_texto=open("archivo.txt","a")
+archivo_texto=open("archivo.txt","r+") # Lectura y escritura
 
-archivo_texto.write("\n Siempre es una buena ocacion para hacer ejercicio")
+#archivo_texto.write("Comienzo del curso")
+
+lista_texto=archivo_texto.readlines()
+
+lista_texto[1]="Esta linea ha sido incluida desde el exterior \n"
+
+archivo_texto.seek(0)
+
+archivo_texto.writelines(lista_texto)
 
 archivo_texto.close()
+
+
 
